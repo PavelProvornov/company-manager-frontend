cd5e79f12bdcf9f537bc4b5eda042f1530ac3a31
PavelProvornov <pavelprovornov@gmail.com>
2023-01-05 00:41:54 +0300

    Main: default file structure created, add dockerfile w/config, add Makefile

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from "react-dom";
 
 import App from './App';
-import "./styles.scss";
+import "./assets/styles/index.scss";
 
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+const mountNode = document.getElementById("app");
+
+ReactDOM.render(<App name="Paul" />, mountNode);
