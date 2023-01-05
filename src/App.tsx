936be669a277ed5a755dff60b89b65b503fec64c
PavelProvornov <pavelprovornov@gmail.com>
2023-01-05 16:57:22 +0300

    Main: add linter, add prettier, add store and basic hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-
 import React, { FC } from 'react';
-import { hot } from "react-hot-loader/root";
+import { hot } from 'react-hot-loader/root';
 
-interface Props {
-   name?: string
-}
-
-const App: React.FC<Props> = () => {
-  return (
-    <div className="app">
-      ssdfsdf
-    </div>
-  );
-}
+const App: FC = () => {
+    return <div className='app'>ssdfsdf</div>;
+};
 
 export default hot(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 
 import App from './App';
-import "./assets/styles/index.scss";
+import './assets/styles/index.scss';
 
-const mountNode = document.getElementById("app");
+const mountNode = document.getElementById('app');
 
-ReactDOM.render(<App/>, mountNode);
+ReactDOM.render(<App />, mountNode);
